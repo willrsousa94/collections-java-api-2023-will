5dc301b78024600f36b20597604c7c55ca4031e3
William Sousa <williamrsousa94@hotmail.com>
2023-10-18 15:18:30 -0300

    finalizado lição de ordenação

--- collections-java-api-2023/src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ collections-java-api-2023/src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -1,4 +1,46 @@
 package main.java.list.Ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoas {
+
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoa = new OrdenacaoPessoas();
+
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.55);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 24, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 38, 2.15);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 15, 1.80);
+
+        //System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+
 }
